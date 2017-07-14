db7218b9caab33124a9627501fa017ba118243ea
Jurandir de Castro Vasconcelos Junior <jcvj@NEUROTECH.INTRANET>
2017-07-14 12:19:17 -0300

    alteracao na porta

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+var port=Number(process.env.PORT || 3000);
+
 app.use(express.static(__dirname + '/static'));
 
-app.listen(process.env.PORT || 8080, () => {
+app.listen(port, () => {
   console.log('Server runing in port 4001');
 });
