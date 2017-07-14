345d0a99b6475e4224aaa8acb788ac9d6de530ed
Gabriel Pereira Ramos <juniorcastrov@gmail.com>
2017-07-14 03:02:32 -0300

    porta

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ const path = require('path');
 
 app.use(express.static(__dirname + '/app'));
 
-app.listen(4001, () => {
+app.listen(process.env.PORT || 4001, () => {
   console.log('Server runing in port 4001');
 });
